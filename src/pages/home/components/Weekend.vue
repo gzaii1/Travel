b0fe9167ab45d9f0e68135760ee12bea30de5cf0
gzaii1 <942096269@qq.com>
2019-05-26 08:52:23 +0800

    com6

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="recommend-title">想去哪玩</div>
         <ul>
-            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <li class="item border-bottom" v-for="item of list" :key="item.id">
                 <div class="item-img-wrapper">
                     <img :src="item.imgUrl" class="item-img">
                 </div>
@@ -18,28 +18,8 @@
 <script>
     export default {
         name: 'HomeWeekend',
-        data () {
-            return {
-            recommendList:[
-                {
-                    id : '0001',
-                    imgUrl : 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-                    title:'大连圣亚海洋世界',
-                    desc:'浪漫大连首站，浪漫的海洋主题乐园'
-                },
-                {
-                    id : '0002',
-                    imgUrl : 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-                    title:'大连圣亚海洋世界',
-                    desc:'浪漫大连首站，浪漫的海洋主题乐园'
-                },
-                {
-                    id : '0003',
-                    imgUrl : 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-                    title:'大连圣亚海洋世界',
-                    desc:'浪漫大连首站，浪漫的海洋主题乐园'
-                }
-            ]}
+        props:{
+            list :Array
         }
     }
 </script>
@@ -47,14 +27,13 @@
 <style lang="stylus" scoped="scoped">
     @import '~styles/mixins.styl'
     .recommend-title
-        margin-top :8px
         line-height:32px
         background:#eee
         text-indent:8px
     .item-img-wrapper
         overflow : hidden
         height:0
-        padding-bottom:33.9%
+        padding-bottom:37.09%
     .item-img
         width:100%
        .item-info
