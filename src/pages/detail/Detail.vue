faa12d7861b02d586d0d6e856f1dcb39eb0fcf76
gzaii1 <942096269@qq.com>
2019-05-26 18:54:30 +0800

    com13

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
-    import DetailBanner from './components/Banner'
-    export default{
-        name : 'Detail',
-        components:{
-            DetailBanner
-        }
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+export default{
+    name : 'Detail',
+    components:{
+        DetailBanner,
+        DetailHeader
     }
+}
 </script>
 
 <style lang="stylus" scoped="scoped">
-    
+    .content
+        height:2500px
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="header-abs">
+            <div class="iconfont header-abs-back" @click="handleBackToHome">&#xe624;</div>
+        </div>
+        <div 
+            class="header-fixed" 
+            v-show="!showAbs"
+            :style="opacityStyle">
+             <router-link to="/">
+                <div class="iconfont header-fixed-back">&#xe624;</div>
+            </router-link>
+            景点详情
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'DetailHeader',
+        methods:{
+            handleBackToHome(){
+                this.$router.push('/')
+            },
+             handleScroll(){
+                const top = document.documentElement.scrollTop
+                if(top >50 ||top <100){
+                    const opacity = top/100
+                    this.opacityStyle ={ opacity }
+                    this.showAbs = false
+                }else{
+                    this.showAbs = true
+                }
+            }
+        },
+        data(){
+            return{
+                showAbs:true,
+                opacityStyle:0
+            }
+        },
+        activated(){
+            window.addEventListener('scroll',this.handleScroll)
+        }
+    }
+</script>
+
+<style lang="stylus" scoped="scoped">
+    @import '~styles/varibles.styl'
+    .header-abs
+        position:absolute
+        left:10px
+        top:10px
+        width:40px
+        height:40px
+        line-height:40px
+        border-radius:20px
+        text-align:center
+        background:rgba(0,0,0,0.8)
+        .header-abs-back   
+            color:#fff
+            font-size:20px
+     .header-fixed
+         position:fixed
+         top:0
+         left:0
+         right:0
+         height:42px
+         line-height:42px
+         text-align:center
+         color:#fff
+         background:$bgColor
+         font-size:17px
+         .header-fixed-back
+            position:absolute
+            top:0
+            left:0
+            width:34px
+            text-align:center
+            font-size:20px
+            color:#fff
+</style>
